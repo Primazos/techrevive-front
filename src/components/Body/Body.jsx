beebc7135b9502edcff5617f0898ac58ee50c60d
Jorge-Guedes <jorgeguedespena@alumno.ieselrincon.es>
2025-02-02 19:15:39 +0000

    diseño home

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const Body = () => {
   return (
-    <div className="flex w-full flex-col">
-      <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+    <div className="flex w-full h-full flex-col gap-16 justify-center bg-red-300">
+      <div className="card bg-base-300 rounded-box w-[80%] self-center grid h-52 place-items-center">
         content
       </div>
-      <div className="divider"></div>
-      <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+      <div className="card bg-base-300 rounded-box w-[80%] self-center grid h-52 place-items-center">
         content
       </div>
     </div>
