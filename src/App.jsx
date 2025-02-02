877077517fdd46c8039ff5416dfab8fc6c5f931b
Jorge-Guedes <jorgeguedespena@alumno.ieselrincon.es>
2025-02-02 18:06:27 +0000

    Se añadio daisyui y varios componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Body from "./components/Body/Body";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="h-screen flex justify-between flex-col">
+      <Navbar />
+      <Body />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div className="flex w-full flex-col">
+      <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+        content
+      </div>
+      <div className="divider"></div>
+      <div className="card bg-base-300 rounded-box grid h-20 place-items-center">
+        content
+      </div>
+    </div>
+  );
+};
+
+export default Body;
