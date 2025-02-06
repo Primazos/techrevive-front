5fbf4fe60f4179175b379e3f8d038ebf3329af57
Jorge-Guedes <jorgeguedespena@alumno.ieselrincon.es>
2025-02-06 19:57:11 +0000

    Proyecto estructurado por ficheros y directorios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Body from "./components/Body/Body";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/layout/Footer/Footer";
+import Navbar from "./components/layout/Navbar/Navbar";
 
 function App() {
   return (
     <div className="h-screen flex justify-between flex-col">
       <Navbar />
-      <Body />
+      <Home />
       <Footer />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Body = () => {
+const Home = () => {
   return (
     <div className="flex w-full h-full flex-col gap-16 justify-center bg-red-300">
       <div className="card bg-base-300 rounded-box w-[80%] self-center grid h-52 place-items-center">
@@ -13,4 +13,4 @@ const Body = () => {
   );
 };
 
-export default Body;
+export default Home;
